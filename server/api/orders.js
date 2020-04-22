a7110726f14d09c3cd141db3f16859597a520016
rodrigojm <rodrigomallmann1@gmail.com>
2020-04-22 16:54:40 -0400

    f04 - added remaining crud routes and checkout route

--- server/api/orders.js
+++ server/api/orders.js
@@ -10,6 +10,66 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:orderId', async (req, res, next) => {
+  try {
+    const foundOrder = await Order.findByPk(req.params.orderId)
+
+    if (foundOrder) {
+      res.status(200).json(foundOrder)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const foundOrder = await Order.findByPk(req.params.orderId)
+
+    if (foundOrder) {
+      const updatedOrder = await foundOrder.update(req.body)
+
+      res.status(200).json(updatedOrder)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newOrder = await Order.create(req.body)
+
+    if (newOrder) {
+      res.status(201).send(newOrder)
+    } else {
+      res.sendStatus(400)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:orderId', async (req, res, next) => {
+  try {
+    const foundOrder = await Order.findByPk(req.params.orderId)
+
+    if (foundOrder) {
+      await foundOrder.destroy()
+
+      res.status(200).json(foundOrder)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
 // this is to find a single active order related to a user
 router.get('/:userId/active', async (req, res, next) => {
   try {
@@ -110,6 +170,28 @@ router.put('/:userId/remove/:hobbyId', async (req, res, next) => {
     }
   } catch (error) {
     next(error)
+  }
+})
+
+router.put('/:userId/checkout', async (req, res, next) => {
+  try {
+    const foundOrder = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        isActive: true
+      },
+      include: [User]
+    })
+
+    if (foundOrder) {
+      await foundOrder.checkoutOrder()
+
+      res.status(200).json(foundOrder)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
